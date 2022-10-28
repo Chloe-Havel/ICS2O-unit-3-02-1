d4bbbe152339e10e22bfd64ec7c9014a5a7c23ea
Chloe-Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-10-28 14:28:37 -0400

    a lot

--- js/script.js
+++ js/script.js
@@ -10,14 +10,22 @@
  * Check servie worker.
  */
 if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test-1/sw.js", {
-    scope: "/ICS2O-PWA-Test-1/",
+  navigator.serviceWorker.register("/ICS2O-unit-3-02-1/sw.js", {
+    scope: "/ICS2O-unit-3-02-1/",
   })
 }
 
 /**
- * This function displays an alert.
+ * This function calculates area and perimeter of rectangle.
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+ function calculate() {
+  // input
+  const Side1 = parseInt(document.getElementById("side-1-of-triangle").value)
+  const Side2 = parseInt(document.getElementById("side-2-of-triangle").value)
+  const Side3 = parseInt(document.getElementById("side-3-of-triangle").value)
+  // process
+  const triangle = Side1 + Side2 + Side3
+
+  // output
+  document.getElementById("area").innerHTML = "perimeter is: " + triangle + " cm "
 }
