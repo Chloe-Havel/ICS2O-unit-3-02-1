302184bd866150014cbff7709cb71ce55634726c
Chloe-Havel <113055595+Chloe-Havel@users.noreply.github.com>
2022-10-28 14:45:25 -0400

    js and html

--- js/script.js
+++ js/script.js
@@ -20,11 +20,11 @@ if (navigator.serviceWorker) {
  */
 function calculate() {
   // input
-  const Side1 = parseInt(document.getElementById("side-1-of-triangle").value)
-  const Side2 = parseInt(document.getElementById("side-2-of-triangle").value)
-  const Side3 = parseInt(document.getElementById("side-3-of-triangle").value)
+  const length = parseInt(document.getElementById("v-length-of-pyramid").value)
+  const width = parseInt(document.getElementById("v-width-of-pyramid").value)
+  const height = parseInt(document.getElementById("-height-of-pyramid").value)
   // process
-  const triangle = Side1 + Side2 + Side3
+  const triangle = length * width * height
 
   // output
   document.getElementById("area").innerHTML =
